b8ffd45a1d766c9e760ece2636e25bfcd60ea763
NikitaDolgopolov <kamikadzni@mail.ru>
2016-11-15 22:39:42 +0400

    Добавил 3 вкладку с сохранением и загрузкой из файла. Пока работает только сохранение по нажатию кнопки и поле для ввода названия.

--- src/com/company/WindowView.java
+++ src/com/company/WindowView.java
@@ -13,6 +13,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import javax.swing.JScrollPane;
@@ -29,6 +30,7 @@ import javax.swing.JButton;
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.event.TableModelEvent;
+import javax.swing.JTextField;
 
 
 
@@ -37,6 +39,16 @@ import javax.swing.event.TableModelEvent;
 public class WindowView extends JFrame implements View{
     private DefaultTableModel b = new DefaultTableModel();
     private DefaultTableModel c = new DefaultTableModel();
+    private DefaultTableModel s = new DefaultTableModel();
+
+    public void print(String s) {
+        System.out.println(s);
+    }
+    public void read() {}
+    public String input() {
+        return "1";
+    }
+    private JTextField saveLine = new JTextField();
 
     /**
      * Конструктор - создание нового объекта
@@ -44,28 +56,60 @@ public class WindowView extends JFrame implements View{
     public WindowView() {          
           
           setTitle("Library");
-
           
-          JPanel panel = new JPanel();
-          add(panel); 
-          panel.setLayout(new BorderLayout());
+          JPanel bookTab = new JPanel();
+          bookTab.setLayout(new BorderLayout());
+          JPanel copyTab = new JPanel();
+          copyTab.setLayout(new BorderLayout());
+          JPanel saveTab = new JPanel();
+          saveTab.setLayout(new BorderLayout());
           JTable book = new JTable(b);
-          JTable copyOfBook = new JTable(c); 
+          JTable copyOfBook = new JTable(c);
+          JTable saveAndLoad = new JTable(s);
+          JScrollPane scrollPaneS = new JScrollPane(saveAndLoad);
           JScrollPane scrollPaneB = new JScrollPane(book);
           JScrollPane scrollPaneC = new JScrollPane(copyOfBook);
-          JPanel panel2 = new JPanel();
-          panel.add(panel2,BorderLayout.SOUTH);
-          panel2.setLayout(new FlowLayout());
+          JPanel bookFunctionButtons = new JPanel();
+          bookFunctionButtons.setLayout(new FlowLayout());
           JButton button1 = new JButton("Add");          
           JButton button2 = new JButton("Change");  
-          panel2.add(button1);
-          panel2.add(button2);
+          bookFunctionButtons.add(button1);
+          bookFunctionButtons.add(button2);
+
+          JPanel copyFunctionButtons = new JPanel();
+          copyFunctionButtons.setLayout(new FlowLayout());
+          JButton addCopyButton = new JButton("Add");
+          JButton changeCopyButton = new JButton("Change");
+          copyFunctionButtons.add(addCopyButton);
+          copyFunctionButtons.add(changeCopyButton);
+
+          JPanel saveAndLoadButtons = new JPanel();
+          saveAndLoadButtons.setLayout(new FlowLayout());
+          JButton saveButton = new JButton("Save");
+          JButton loadButton = new JButton("Load");
+          saveAndLoadButtons.add(saveButton);
+          saveAndLoadButtons.add(loadButton);
+
+
+          saveButton.addActionListener(new ActionListener() {
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  saveToFile();
+              }
+          });
+          saveLine = new JTextField();
+
+          bookTab.add(bookFunctionButtons,BorderLayout.SOUTH);
+          copyTab.add(copyFunctionButtons,BorderLayout.SOUTH);
+          saveTab.add(saveAndLoadButtons,BorderLayout.SOUTH);
+          saveTab.add(saveLine,BorderLayout.NORTH);
           JTabbedPane jtp = new JTabbedPane();
-         
-          
-          jtp.addTab("Book",scrollPaneB );
-          jtp.addTab("CopyOfBook", scrollPaneC);
-          panel.add(jtp,BorderLayout.CENTER);       
+          bookTab.add(scrollPaneB,BorderLayout.CENTER);
+          copyTab.add(scrollPaneC,BorderLayout.CENTER);
+          saveTab.add(scrollPaneS,BorderLayout.CENTER);
+          jtp.addTab("Book", bookTab);
+          jtp.addTab("CopyOfBook", copyTab);
+          jtp.addTab("Save and Load", saveTab);
           
             
           b.addColumn("Name");
@@ -76,8 +120,12 @@ public class WindowView extends JFrame implements View{
           c.addColumn("Inventory Number");
           c.addColumn("Book ID");
           c.addColumn("Issue");
-            
-     }
+
+          s.addColumn("File Name");
+
+          add(jtp);
+
+    }
 
     /**
      * Процедура добавления в таблицу одной книги
@@ -116,7 +164,7 @@ public class WindowView extends JFrame implements View{
             newRow.add(tmp.getAuthors());
             newRow.add(Integer.toString(tmp.getYear()));
             newRow.add(Integer.toString(tmp.getPages()));
-            b.addRow(newRow);
+            c.addRow(newRow);
         }
     }
 
@@ -131,15 +179,44 @@ public class WindowView extends JFrame implements View{
             newRow.add(Long.toString(tmp.getInventoryNumber()));
             newRow.add(Long.toString(tmp.getIdBook()));
             newRow.add(Boolean.toString(tmp.getIssue()));
-            b.addRow(newRow);
+            c.addRow(newRow);
+        }
+    }
+
+    public void saveToFile() {
+        if (saveLine.getText() != "") {
+            Serialization a = new Serialization();
+            try {
+                Book tmp = new Book();
+                for (int i = 0; i < b.getRowCount(); i++) {
+                    tmp.setName((String) b.getValueAt(i, 1));
+                    tmp.setAuthors((String) b.getValueAt(i, 2));
+                    tmp.setYear((Integer) b.getValueAt(i, 3));
+                    tmp.setPages((Integer) b.getValueAt(i, 4));
+                    a.saveObjectBook(tmp,saveLine.getText()+"Book.txt");
+                }
+
+
+                CopyOfTheBook temp= new CopyOfTheBook();
+                for (int i = 0; i < c.getRowCount(); i++) {
+                    temp.setIdBook((Long) c.getValueAt(i, 1));
+                    temp.setInventoryNumber((Long) c.getValueAt(i, 2));
+                    temp.setIssue((Boolean) b.getValueAt(i, 3));
+                    a.saveObjectCopy(temp,saveLine.getText()+"CopyOfTheBook.txt");
+                }
+
+
+
+            }
+            catch (IOException ex) {
+            }
         }
     }
 
     public static void main(String[] args) {
-      WindowView w = new WindowView();
-      w.setBounds(400, 400, 400, 200);
-      w.setVisible(true);
-
+        WindowView w = new WindowView();
+        w.setBounds(400, 400, 400, 200);
+        w.setVisible(true);
     }
      
 }
